3ee72b835503800ef64ac5777fcbfcd2f62cfadc
votafore <a.zakharov.druzh@gmail.com>
2018-07-05 16:01:44 +0300

    save 1

---
+++ app/src/main/java/com/votafore/organizer/system/presenters/PageMainPresenter.kt
@@ -0,0 +1,4 @@
+package com.votafore.organizer.system.presenters
+
+class PageMainPresenter {
+}
--- app/src/main/java/com/votafore/organizer/ui/pages/PageMain.kt
+++ app/src/main/java/com/votafore/organizer/ui/pages/PageMain.kt
@@ -1,15 +1,39 @@
 package com.votafore.organizer.ui.pages
 
 import android.os.Bundle
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.votafore.organizer.R
+import kotlinx.android.synthetic.main.fragment_main.view.*
+import android.support.v7.widget.PagerSnapHelper
+
+
 
 class PageMain: PageBase() {
 
 
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        return inflater!!.inflate(R.layout.fragment_test, container, false)
+
+        val root = inflater!!.inflate(R.layout.fragment_test, container, false)
+
+        val list = root.list_events.apply {
+            layoutManager = LinearLayoutManager(activity, RecyclerView.HORIZONTAL, false)
+            itemAnimator = DefaultItemAnimator()
+            // TODO; set adapter
+
+            // https://www.linkedin.com/pulse/create-viewpager-using-recyclerview-android-ali-ahmed/
+            // https://blog.davidmedenjak.com/android/2017/06/24/viewpager-recyclerview.html
+
+            // add pager behavior
+            val snapHelper = PagerSnapHelper()
+            snapHelper.attachToRecyclerView(this)
+        }
+
+        return root
     }
 }
