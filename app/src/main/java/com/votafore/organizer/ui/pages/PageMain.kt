8572abd55f43d912f401b0e7b5fc5514efcb08bc
votafore <a.zakharov.druzh@gmail.com>
2018-07-05 22:21:15 +0300

    navigation ready 2

--- app/src/main/java/com/votafore/organizer/ui/pages/PageMain.kt
+++ app/src/main/java/com/votafore/organizer/ui/pages/PageMain.kt
@@ -3,19 +3,16 @@ package com.votafore.organizer.ui.pages
 import android.os.Bundle
 import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.PagerSnapHelper
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.votafore.organizer.R
 import kotlinx.android.synthetic.main.fragment_main.view.*
-import android.support.v7.widget.PagerSnapHelper
-
 
 
 class PageMain: PageBase() {
-
-
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
 
