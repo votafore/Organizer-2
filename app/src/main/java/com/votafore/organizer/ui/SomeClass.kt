b64fe537f9ca56faa3b00b7cea7354ddbc2eb3ff
votafore <a.zakharov.druzh@gmail.com>
2018-07-06 14:23:08 +0300

    content changed

---
+++ app/src/main/java/com/votafore/organizer/ui/SomeClass.kt
@@ -0,0 +1,11 @@
+package com.votafore.organizer.pages
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.votafore.organizer.R
+
+class SomeClass {
+
+}
